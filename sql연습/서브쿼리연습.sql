de583d30df166f13250ee96d374bf606c91df71a
hanriverkitty <danieltls@naver.com>
2024-05-16 10:47:17 +0900

    단일행 서브쿼리 fin

--- sql연습/서브쿼리연습.sql
+++ sql연습/서브쿼리연습.sql
@@ -56,8 +56,81 @@ WHERE
             a.emp_no = b.emp_no
                 AND b.to_date = '9999-01-01'
                 AND CONCAT(a.first_name, ' ', a.last_name) = 'Fai Bale');
-                
--- 3-1) 단일행 연산자: =, >, <, >=, <=, <>, !=
--- 3-2) 복수행 연자: in, not in, 비교연산자any, 비교연산지all
+
 -- where 서브쿼리의 결과가 복수 열일 경우 ()안에 이름을 넣어 맞출 수 있다
--- (colunmn1, column2) = 서브쿼리 결과1, 서브쿼리 결과2
+-- (colunmn1, column2) = 서브쿼리 결과1, 서브쿼리 결과2                
+-- 3-1) 단일행 연산자: =, >, <, >=, <=, <>, !=
+
+-- 실습문제1
+-- 현재, 전체 사원의 평균 연봉보다 적은 급여를 받는 사원의 이름과 급여를 출력 하세요.
+select a.first_name, b.salary
+from employees a, salaries b
+where a.emp_no = b.emp_no
+and b.to_date = '9999-01-01'
+and b.salary < (select avg(salary)
+				from salaries
+				where to_date = '9999-01-01')
+order by b.salary desc;
+
+-- 실습문제2
+-- 현재, 직책별 평균급여 중에 가장 작은 직책의 직책이름과 그 평균 급여를 출력해 보세요.
+-- 집계함수는 다른 컬럼이 오면 안된다
+-- 틀린 방법
+-- select avg_t.title,min(avg_sal)
+-- from (
+-- 	select a.title, avg(b.salary) as avg_sal
+-- 	from titles a, salaries b
+-- 	where a.emp_no = b.emp_no
+-- 	and a.to_date = '9999-01-01' 
+-- 	and b.to_date = '9999-01-01'
+-- 	group by a.title) as avg_t;
+    
+-- 3) sol1: where 절 subquery
+select a.title, avg(b.salary)
+from titles a, salaries b
+where a.emp_no = b.emp_no
+and a.to_date = '9999-01-01' 
+and b.to_date = '9999-01-01'
+group by a.title 
+having avg(b.salary) = (select min(avg_sal)
+							from (
+								select a.title, avg(b.salary) as avg_sal
+								from titles a, salaries b
+								where a.emp_no = b.emp_no
+								and a.to_date = '9999-01-01' 
+								and b.to_date = '9999-01-01'
+								group by a.title) as avg_t);
+
+-- 4) sol2: top-k(limit)
+select a.title, avg(b.salary)
+from titles a, salaries b
+where a.emp_no = b.emp_no
+and a.to_date = '9999-01-01' 
+and b.to_date = '9999-01-01'
+group by a.title
+order by avg(b.salary) asc
+limit 0,1;
+-- limit 앞은 인덱스 (0부터 시작) , 뒤는 갯수
+
+-- 3-2) 복수행 연자: in, not in, 비교연산자any, 비교연산지all
+
+-- any 사용법
+-- 1. =any : in
+-- 2. >any, >=any: 최솟값
+-- 3. <any, <=any: 최댓값
+-- 4. <>any, !=any: not in
+
+-- all 사용법
+-- 1. =all: (x) 논리적으로 될 수 가 없다
+-- 2. >all, >=all: 최댓값
+-- 3. <all, <=all: 최솟값
+-- 4. <>all, !=all
+
+-- 실습문제3
+-- 현재 급여가 50000 이상인 직원의 이름과 급여를 출력하세요.
+-- 둘리 60000
+-- 또치 80000
+
+-- sol1) join
+-- sol2) subquery: where(in)
+-- sol3) subquery: where(=any)
